07b0942b0f626476fca8f735b85be0698f26a93c
ANOKYEWAA AKONNOR <anokyewaaa467@gmail.com>
2023-12-10 18:03:27 +0000

    Added files and landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import LandingPage from '../src/components/LandingPage';
+import LoginPage from './components/loginpage';
+import './App.css'; // Link your global CSS file
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/LandingPage" exact component={LandingPage} />
+    //     <Route path="/loginPage" exact component={loginPage}/>
+    //     {/* Add routes for other pages */}
+    //   </Switch>
+    // </Router>
+    <BrowserRouter>
+      <LandingPage />
+      <LoginPage/>
+    </BrowserRouter>
   );
 }
 
